fccb141d05144d3cfaddafbcc9136a1cae715c4d
barandenizkorkmaz <barandenizkorkmaz@gmail.com>
2024-11-29 01:00:21 +0100

    update solutions

---
+++ src/stackandqueue/_20_ValidParantheses.java
@@ -0,0 +1,29 @@
+package stackandqueue;
+
+import java.util.Stack;
+
+public class _20_ValidParantheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty())return false;
+                if(c == ')' && stack.pop() != '('){
+                    return false;
+                }
+                else if(c == '}' && stack.pop() != '{'){
+                    return false;
+                }
+                else if(c == ']' && stack.pop() != '['){
+                    return false;
+                }
+
+            }
+        }
+        return stack.isEmpty();
+    }
+}

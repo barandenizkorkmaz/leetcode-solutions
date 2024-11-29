dedf7672c2b2724501d8a6ea5514201d0209012e
barandenizkorkmaz <barandenizkorkmaz@gmail.com>
2024-11-29 16:54:13 +0100

    update solutions

--- src/recursion/_23_GenerateParantheses.java
+++ src/recursion/_23_GenerateParantheses.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class _23_GenerateParantheses {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new LinkedList<String>();
+        List<String> res = new LinkedList<>();
         recurse(res, 0, 0, "", n);
         return res;
     }

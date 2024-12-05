72f8ee9b62894d87296e631344c7e35dbba5d264
barandenizkorkmaz <barandenizkorkmaz@gmail.com>
2024-12-05 15:56:15 +0100

    update solutions

--- src/dp/_746_MinCostClimbingStairs.java
+++ src/dp/_746_MinCostClimbingStairs.java
@@ -11,4 +11,18 @@ public class _746_MinCostClimbingStairs {
         }
         return Math.min(costs[n-1], costs[n-2]);
     }
+
+    /*
+
+    For me, below solution is more intuitive to understand!
+
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost.length == 2)return Math.min(cost[0], cost[1]);
+        int[] dp = new int[cost.length + 1];
+        for(int i = 2; i < cost.length + 1; i++){
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+        return dp[cost.length];
+    }
+     */
 }
